5ec01ac3a35c8b744b5a30d835e8bee4be65edeb
Karel Kroeze <k.a.kroeze@utwente.nl>
2024-12-05 10:17:59 +0100

    decouple header level from styling in author-list include, stop using header elements for ", and" etc

--- assets/js/pages/people.js
+++ assets/js/pages/people.js
@@ -1,95 +1,111 @@
-
-const token="c2VjcmV0LXRva2VuOjcwNWE0OTQyNGE3OTRiNTM1Mzc2MzE2YzQ2NGI1OTYzNzI3NTcwNzc1NjUx"
-const localStorageKey="bdsi.people"
-const localStorageKeyVersion=2
+const token =
+  "c2VjcmV0LXRva2VuOjcwNWE0OTQyNGE3OTRiNTM1Mzc2MzE2YzQ2NGI1OTYzNzI3NTcwNzc1NjUx";
+const localStorageKey = "bdsi.people";
+const localStorageKeyVersion = 2;
 
 /**
  * Call the UTwente people API to attempt to resolve non-BDSi people mentioned in projects, workshops, etc.
- * 
+ *
  * @param {string} name search query for the people API
  * @returns {{ found: number, data: any[]}}
  */
-const resolve = async name => {
-    // try resolve from browser storage
-    let person = localStorage.getItem(`${localStorageKey}.v${localStorageKeyVersion}.${name}`);
-    if (person) {
-        person = JSON.parse(person);
-        console.debug({ name, data: person }, "resolved from cache");
-        return person;
-    } 
+const resolve = async (name) => {
+  // try resolve from browser storage
+  let person = localStorage.getItem(
+    `${localStorageKey}.v${localStorageKeyVersion}.${name}`
+  );
+  if (person) {
+    person = JSON.parse(person);
+    console.debug({ name, data: person }, "resolved from cache");
+    return person;
+  }
 
-    // try resolve from people API
-    const response = await fetch(`https://people.utwente.nl/peoplepagesopenapi/contacts?authorization=${atob(token)}&query=${name}`);
-    data = await response.json();
-    if (data.found) {
-        // console.log({name, data});
+  // try resolve from people API
+  const response = await fetch(
+    `https://people.utwente.nl/peoplepagesopenapi/contacts?authorization=${atob(
+      token
+    )}&query=${name}`
+  );
+  data = await response.json();
+  if (data.found) {
+    // console.log({name, data});
 
-        // consider first result only
-        person = data.data[0]
-        // console.log({person})
+    // consider first result only
+    person = data.data[0];
+    // console.log({person})
 
-        // check for at least a partial match
-        if (stringSimilarity(name, person.givenName + " " + person.displayName.split(",", 1).join("")) >= 0.5) {
-            localStorage.setItem(`${localStorageKey}.v${localStorageKeyVersion}.${name}`, JSON.stringify(person));
-            console.debug({ name, person }, "resolved from people API");
-            return person;
-        }
-    } 
+    // check for at least a partial match
+    if (
+      stringSimilarity(
+        name,
+        person.givenName + " " + person.displayName.split(",", 1).join("")
+      ) >= 0.5
+    ) {
+      localStorage.setItem(
+        `${localStorageKey}.v${localStorageKeyVersion}.${name}`,
+        JSON.stringify(person)
+      );
+      console.debug({ name, person }, "resolved from people API");
+      return person;
+    }
+  }
 
-    // we failed...
-    console.debug({ name, person, data }, "could not be resolved")
-}
+  // we failed...
+  console.debug({ name, person, data }, "could not be resolved");
+};
 
 /**
  * Find all unresolved people mentioned on the page and call {resolve} on each.
  */
 const resolveAll = async () => {
-    /** 
-    * @type {IterableIterator<HTMLDivElement>} 
-    * */
-    const people = document.querySelectorAll(".author-list-item.unresolved").values();
-    for (const personElement of people) {
-        const person = await resolve(personElement.dataset["name"])
-        if (person){ 
-            hydrate(person, personElement)
-        }
+  /**
+   * @type {IterableIterator<HTMLDivElement>}
+   * */
+  const people = document
+    .querySelectorAll(".author-list-item.unresolved")
+    .values();
+  for (const personElement of people) {
+    const person = await resolve(personElement.dataset["name"]);
+    if (person) {
+      hydrate(person, personElement);
     }
-}
+  }
+};
 
 /**
  * Hydrate an unresolved people mention with additional information as available:
- * 
+ *
  * - link to people page
  * - avatar image
- * 
- * @param {{profileUrl: string, pictureUrl: string}} person person data returned from {resolve} 
+ *
+ * @param {{profileUrl: string, pictureUrl: string}} person person data returned from {resolve}
  * @param {HTMLDivElement} element html div element to hydrate
  */
 const hydrate = (person, element) => {
-    // create anchor tag
-    const anchor = document.createElement("a");
-    anchor.href = person.profileUrl;
-    anchor.target = "_blank";
-    anchor.classList.add("author-list-item", "external")
-    element.classList.remove("author-list-item", "unresolved")
+  // create anchor tag
+  const anchor = document.createElement("a");
+  anchor.href = person.profileUrl;
+  anchor.target = "_blank";
+  anchor.classList.add("author-list-item", "external");
+  element.classList.remove("author-list-item", "unresolved");
 
-    // insert anchor tag after the original element, then append (move) the element into anchor
-    element.after(anchor);
-    anchor.append(element);
+  // insert anchor tag after the original element, then append (move) the element into anchor
+  element.after(anchor);
+  anchor.append(element);
 
-    // create and insert profile image
-    if (person.pictureUrl) {
-        const image = document.createElement("img");
-        image.src = person.pictureUrl;
-        image.alt = person.displayName;
+  // create and insert profile image
+  if (person.pictureUrl) {
+    const image = document.createElement("img");
+    image.src = person.pictureUrl;
+    image.alt = "";
 
-        element.prepend(image);
-    }
-}
+    element.prepend(image);
+  }
+};
 
 /**
  * Calculate similarity between two strings
- * 
+ *
  * Copied from https://github.com/stephenjjbrown/string-similarity-js.
  * @license MIT
  * @param {string} str1 First string to match
@@ -99,31 +115,32 @@ const hydrate = (person, element) => {
  * @returns Number between 0 and 1, with 0 being a low match score.
  */
 var stringSimilarity = function (str1, str2, substringLength, caseSensitive) {
-    if (substringLength === void 0) { substringLength = 2; }
-    if (caseSensitive === void 0) { caseSensitive = false; }
-    if (!caseSensitive) {
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
+  if (substringLength === void 0) {
+    substringLength = 2;
+  }
+  if (caseSensitive === void 0) {
+    caseSensitive = false;
+  }
+  if (!caseSensitive) {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+  }
+  if (str1.length < substringLength || str2.length < substringLength) return 0;
+  var map = new Map();
+  for (var i = 0; i < str1.length - (substringLength - 1); i++) {
+    var substr1 = str1.substr(i, substringLength);
+    map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
+  }
+  var match = 0;
+  for (var j = 0; j < str2.length - (substringLength - 1); j++) {
+    var substr2 = str2.substr(j, substringLength);
+    var count = map.has(substr2) ? map.get(substr2) : 0;
+    if (count > 0) {
+      map.set(substr2, count - 1);
+      match++;
     }
-    if (str1.length < substringLength || str2.length < substringLength)
-        return 0;
-    var map = new Map();
-    for (var i = 0; i < str1.length - (substringLength - 1); i++) {
-        var substr1 = str1.substr(i, substringLength);
-        map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
-    }
-    var match = 0;
-    for (var j = 0; j < str2.length - (substringLength - 1); j++) {
-        var substr2 = str2.substr(j, substringLength);
-        var count = map.has(substr2) ? map.get(substr2) : 0;
-        if (count > 0) {
-            map.set(substr2, count - 1);
-            match++;
-        }
-    }
-    return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
+  }
+  return (match * 2) / (str1.length + str2.length - (substringLength - 1) * 2);
 };
 
-
-
-resolveAll()
+resolveAll();
